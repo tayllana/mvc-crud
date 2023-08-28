ac66e7700463fb00f0ebf08060ea21dad7d06245
Tayllana Wislla <tayllanawislla@gmail.com>
2023-08-28 15:31:52 -0300

    Update .htaccess, UsuariosController.php, and add.php

--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -7,8 +7,8 @@ use src\models\Test;
 class UsuariosController extends Controller {
 
     public function add() {
-        echo 'novo';
-        // $this->render('home', ['nome' => 'Bonieky']);
+        // echo 'novo';
+        $this->render('add');
     }
 
 }
---
+++ src/views/pages/add.php
@@ -0,0 +1,8 @@
+<?php $render('header'); ?>
+
+
+<div class="container">
+    novo
+</div>
+
+<?php $render('footer'); ?>
